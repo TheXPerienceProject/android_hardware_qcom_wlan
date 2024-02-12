8a1f5f784eab36d5a56d6c02e7e966572da485f4
klozz <carlosj@klozz.dev>
2024-02-12 08:17:45 +0000

    enforce build
    Change-Id: Ia9cbf99df468a39b7c296a1f64592fe7db3c52fc
    Signed-off-by: klozz <carlosj@klozz.dev>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-wlan))
+LOCAL_PATH := $(call my-dir)
+#ifeq ($(call my-dir),$(call project-path-for,qcom-wlan))
+$(warning calling wlan )
+#ifeq ($(call my-dir),$(call project-path-for,qcom-wlan))
 
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 QCOM_WLAN_ROOT := $(call my-dir)
@@ -7,4 +10,4 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
 endif
 
-endif
+#endif
